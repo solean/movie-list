7bff582be5e7139c7380df04cb3453103a0995b3
Chris Schnabel <chrisjschnabel@gmail.com>
2016-03-06 00:19:17 -0500

    Working prototype, keep list of movies in movies.txt, app displays info using omdb api.

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
+var fs = require('fs');
 var omdb = require('omdb-node-wrapper');
 var app = express();
 
-app.engine('handlebars', exphbs({defaultLayout: 'home'}));
-app.set('view engine', 'handlebars');
 
+this.movieList = [];
+
+// Read list of line separated movies from movies.txt
+fs.readFile('movies.txt', 'utf-8', (err, data) => {
+  if (err) { throw err; }
+  var lines = data.split('\n');
+  lines.pop();
+  this.movieList = lines;
+});
+
+var hbs = exphbs.create({
+  helpers: {
+    generateMovieUrl: title => 'http://localhost:3000/movie/' + title
+  }
+});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
-  // Replace Alien with a req param (or path param)
-  omdb.getMovieByTitle('Alien', {}, function(data) {
+
+app.get('/', (req, res) => res.redirect('/movie/Alien'));
+
+app.get('/movie/:title', (req, res) => {
+  omdb.getMovieByTitle(decodeURI(req.params.title), {}, data => {
     var parsedData = JSON.parse(data);
     // Get full res poster
     parsedData.Poster = parsedData.Poster.slice(0, -7) + '1400.jpg';
-
     parsedData.Writer = parsedData.Writer.split(',');
     parsedData.Actors = parsedData.Actors.split(',');
 
     res.render('home', {
       layout: false,
-      movies: ['Alien', 'Gone Girl', 'Blade Runner', 'Scream', 'Sicario'],
+      movies: this.movieList,
       movie: parsedData
     });
   });
